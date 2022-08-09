9476a640bdb55ef2b754336ab2a96c549b050c72
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-08-09 14:00:35 +0200

    fix on axios on pokemon list all151 first

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,7 +6,7 @@ app.get("/first151", async (req, res) => {
   const response = await axios.get(
     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151"
   )
-  res.json(response.data)
+  res.json(response.data.results)
 })
 
 app.get("/:id", async (req, res) => {
